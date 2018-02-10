bcc0f48620559fa3d4fbd02234fec49659313894
Raitis Cerkasovs <raitis.cerkasovs@bbc.co.uk>
2018-02-10 23:28:47 +0000

    Update to prod db

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
         port: 3004
     },
     db: {
-        domain: "@ds143774.mlab.com:43774/",
-        name: "betfair-db",
+        domain: "@ds231938-a0.mlab.com:31938,ds231938-a1.mlab.com:31938/",
+        name: "betfair-db?replicaSet=rs-ds231938",
         login: secret.db.login,
         password: secret.db.password
     },
