0ee1acca79ff32a8bf01eff3ce7acc30e0768771
Raitis Cerkasovs <raitis.cerkasovs@bbc.co.uk>
2018-02-19 01:45:11 +0000

    Update MongoDB to AWS

--- config/config.js
+++ config/config.js
@@ -5,8 +5,10 @@ module.exports = {
         port: 3004
     },
     db: {
-        domain: "@ds231938-a0.mlab.com:31938,ds231938-a1.mlab.com:31938/",
-        name: "betfair-db?replicaSet=rs-ds231938",
+//         domain: "@ds231938-a0.mlab.com:31938,ds231938-a1.mlab.com:31938/",
+//         name: "betfair-db?replicaSet=rs-ds231938",
+        domain: "@raitis.co.uk:27017/",
+        name: "betfair-db",
         login: secret.db.login,
         password: secret.db.password
     },
